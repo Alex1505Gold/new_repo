5a3cb4fba403828089202944d33f9cc020d8a0ca
Alex1505Gold <sgolenkov2002@gmail.com>
2021-04-24 20:11:19 +0300

    add name

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	cout << "Hello world";
+	string name;
+	getline(cin, name);
+	cout << "Hello world " << name;
 	return 0;
 }
