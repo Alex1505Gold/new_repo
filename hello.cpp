3a686f672e05f8b3758f0eda1a28c93bd7437f0e
Alex1505Gold <49805582+Alex1505Gold@users.noreply.github.com>
2021-04-24 23:10:41 +0300

    change comment

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 	std::string name;// user name
-	std::getline(std::cin, name); // read user's name
+	std::getline(std::cin, name); // read user's name!!!!
 	std::cout << "Hello world " << name;
 	return 0;
 }
